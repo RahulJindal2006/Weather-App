299af08f0ea86cc4ad76eb317435674a47ff9e44
Rahul Jindal <thejindalrahul@gmail.com>
2025-01-29 22:13:54 -0500

    Update vite.config.js with correct base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/weather-application/", // Update with your repository name
+  base: "/Weather-App/", // Update with your repository name
 })
